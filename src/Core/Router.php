37230f2593af14afd65a5ef90f08d35a3859cbf3
ahmadspierij <ahmadspierij@gmail.com>
2026-03-18 17:47:41 -0400

    initilized and started  Router file scr/Core/Router.php

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -5,3 +5,38 @@ declare(strict_types=1);
 namespace CourseCompanion\Core;
 
 use RuntimeExecption;
+
+class Router
+{
+	/**
+	 * All registered routes.
+	 */
+	
+	private array $routes = [];
+
+	private int $lastRouteIndex = -1;
+
+
+	private array $middlewareMap = [];
+
+	public function get(string $pattern, mixed $handler): static
+	{
+		return $this->addRoute('POST', $pattern, $handler);
+	}
+
+
+	//========== Private Helpers =======//
+	private function addRoute(string $method, string $pattern, mixed $handler) : static
+	{
+		$this->routes[] = [
+			'method' => strtoupper($method),
+			'pattern' => $pattern,
+			'handler' => $handler,
+			'middleware' => [],
+		]; 
+
+		$this->lastRouteIndex = count($this->routes) - 1;
+
+		return $this;
+	}
+}
